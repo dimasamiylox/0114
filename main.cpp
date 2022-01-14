dce783edfc2ff9d91b45012a9359cd128fc81d6e
Andrey Belan <gusarday@mail.ru>
2022-01-14 11:23:18 +0700

    структуры v2

--- main.cpp
+++ main.cpp
@@ -8,18 +8,33 @@ struct Date {
     int year;
 };
 
+// функция для вывода даты
+void print(Date d) {
+    std::cout << d.day << "." << d.month << "." << d.year << std::endl;
+}
+
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
     // создаем объект
     Date x;
+    Date tommorow;
+
     // инициализирпуем поля
     x.day = 14;
     x.month = 1;
     x.year = 2022;
+
+    tommorow.day = x.day + 1;
+    tommorow.month = 1;
+    tommorow.year = 2022;
+
     // выводим на экран
-    std::cout << x.day << "." << x.month << "." << x.year << std::endl;
+    print(x);
+    print(tommorow);
 
     return a.exec();
+
+
 }
