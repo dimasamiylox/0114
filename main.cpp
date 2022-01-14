397c1d0f142e57410f6adc96d00dd5579d8a308e
Andrey Belan <gusarday@mail.ru>
2022-01-14 10:45:48 +0700

    структуры

--- main.cpp
+++ main.cpp
@@ -1,8 +1,25 @@
 #include <QCoreApplication>
+#include <iostream>
+
+// описание структуры
+struct Date {
+    int day;
+    int month;
+    int year;
+};
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+
+    // создаем объект
+    Date x;
+    // инициализирпуем поля
+    x.day = 14;
+    x.month = 1;
+    x.year = 2022;
+    // выводим на экран
+    std::cout << x.day << "." << x.month << "." << x.year << std::endl;
 
     return a.exec();
 }
